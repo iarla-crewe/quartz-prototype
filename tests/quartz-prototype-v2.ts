5612f30d6f4d2eda1d441bee2f673897b8460ed7
iarla.crewe <iarla.crewe@gmail.com>
2023-08-14 22:01:22 +0100

    Add transfer_lamports test

--- tests/quartz-prototype-v2.ts
+++ tests/quartz-prototype-v2.ts
@@ -3,6 +3,12 @@ import { Program } from "@coral-xyz/anchor";
 import { QuartzPrototypeV2 } from "../target/types/quartz_prototype_v2";
 import { utf8 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
 import { assert, expect } from "chai";
+import { 
+  Keypair, 
+  LAMPORTS_PER_SOL,
+  Transaction,
+  SystemProgram
+} from "@solana/web3.js"
 
 describe("quartz-prototype-v2", () => {
   // Configure the client to use the local cluster.
@@ -28,7 +34,34 @@ describe("quartz-prototype-v2", () => {
   })
 
   it("transfer_lamports", async () => {
-    assert.fail(0, 1, "Not implemented")
+    const destinationAccount = Keypair.generate()
+    expect(
+      await provider.connection.getBalance(destinationAccount.publicKey)
+    ).to.equal(0);
+
+    // Send SOL to vaultPDA
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: provider.wallet.publicKey,
+        toPubkey: vaultPDA,
+        lamports: LAMPORTS_PER_SOL * 2,
+      }),
+    );
+    await provider.sendAndConfirm(transaction);
+
+    // Call PDA to send SOL to destinationAccount
+    const tx = await program.methods
+      .transferLamprts(new anchor.BN(LAMPORTS_PER_SOL))
+      .accounts({
+        sendingWallet: vaultPDA, 
+        receiver: destinationAccount.publicKey
+      })
+      .rpc()
+
+    // Check SOL is received
+    expect(
+      await provider.connection.getBalance(destinationAccount.publicKey)
+    ).to.equal(LAMPORTS_PER_SOL);
   })
 
   it("init_spl", async () => {
