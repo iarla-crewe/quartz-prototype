65b672676c876de6ae25d0ff7c75a1c39f612a91
Iarla Crewe <44999892+iarla-crewe@users.noreply.github.com>
2023-08-23 15:50:34 +0100

    Add dummy data to HomeScreen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,9 +1,17 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function HomeScreen() {
     return (
         <View>
+            <View>
+                <Text>SOL</Text>
+                <Text>12.4</Text>
+            </View>
 
+            <View>
+                <Text>USDC</Text>
+                <Text>40</Text>
+            </View>
         </View>
     );
 }
