3acf5600e56b9695e026580ca1943b17bfeaa735
Nelo0 <83479019+Nelo0@users.noreply.github.com>
2023-09-05 20:39:03 +0100

    Add Firebase Cloud messaging configuarations and handlers

--- App.tsx
+++ App.tsx
@@ -1,18 +1,37 @@
+/* eslint-disable prettier/prettier */
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/ui/screens/HomeScreen';
 import TransferScreen from './src/ui/screens/transfer/TransferScreen';
 import TransferSelectScreen from './src/ui/screens/transfer/TransferSelectScreen';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaView } from 'react-native';
 import TransferConfirmedScreen from './src/ui/screens/transfer/TransferConfirmedScreen';
 import SpendAcceptedScreen from './src/ui/screens/spend/SpendAcceptedScreen';
 import SpendScreen from './src/ui/screens/spend/SpendScreen';
 import SpendDeclinedScreen from './src/ui/screens/spend/SpendDeclinedScreen';
 
+import { Alert } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+import { notification, requestUserPermission, getToken } from './src/utils';
+
 const Stack = createNativeStackNavigator();
 
 export default function App(): JSX.Element {
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
+
+  useEffect(() => {
+    requestUserPermission();
+    notification();
+    getToken();
+  }, []);
+
   return (
     <SafeAreaView style={{height: "100%"}}>
       <NavigationContainer>
--- src/utils.tsx
+++ src/utils.tsx
@@ -1,5 +1,46 @@
+import messaging from '@react-native-firebase/messaging';
+
 export function currencyToString(rawAmount: number, decimals: number) {
-    return (
-        rawAmount / (10 ** decimals)
-    ).toFixed(decimals);    
+  return (rawAmount / 10 ** decimals).toFixed(decimals);
 }
+
+export async function requestUserPermission() {
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+  if (enabled) {
+    console.log('Authorization status:', authStatus);
+  }
+}
+
+export const notification = async () => {
+  // Assume a message-notification contains a "type" property in the data payload of the screen to open
+
+  messaging().onNotificationOpenedApp(remoteMessage => {
+    console.log(
+      'Notification caused app to open from background state:',
+      remoteMessage.notification,
+    );
+  });
+
+  // Check whether an initial notification is available
+  messaging()
+    .getInitialNotification()
+    .then(remoteMessage => {
+      if (remoteMessage) {
+        console.log(
+          'Notification caused app to open from quit state:',
+          remoteMessage.notification,
+        );
+      }
+    });
+};
+
+export const getToken = async () => {
+  await messaging().registerDeviceForRemoteMessages();
+  const token = await messaging().getToken();
+  // save the token to the db
+  console.log(token);
+};
