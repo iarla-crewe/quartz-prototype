08b3f4d3f3671d848866afa15bb83dc93769465d
Iarla Crewe <44999892+iarla-crewe@users.noreply.github.com>
2023-09-13 17:25:34 +0100

    Add placeholder API call

--- App.js
+++ App.js
@@ -1,27 +1,54 @@
-import { StyleSheet, View, Button } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Image } from 'expo-image';
 import { Audio } from 'expo-av';
 import { useEffect, useState } from 'react';
 
 export default function App() {
-  // TODO - Implement server call code
+  const [ showConfirm, setShowConfirm ] = useState(false);
+
+  const playBeep = async () => {
+    const { sound } = await Audio.Sound.createAsync( require('./assets/beep.wav'));
+    await sound.playAsync();
+    sound.unloadAsync();
+  }
+
+  const callAPI = async () => {
+    try {
+      const response = await fetch('https://quartzpay.io/api-demo', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json'
+        },
+        body: JSON.stringify({
+          firstParam: 'yourValue',
+          secondParam: 'yourOtherValue',
+        }),
+      });
+
+      // TODO - Handle response
+      console.log(`Response: ${response}`);
+
+      setShowConfirm(true);
+      playBeep();
+
+    } catch (err) {
+      console.log(`Error: ${err}`);
+    }
+  }
 
   useEffect(() => {
-    async function playBeep() {
-      const { sound } = await Audio.Sound.createAsync( require('./assets/beep.wav'));
-      await sound.playAsync();
-      sound.unloadAsync();
-    }
-    playBeep();
+    callAPI();
   }, []);
 
   return (
     <View style={styles.container}>
-      <Image
-        style={styles.image}
-        source={require('./assets/green_checkmark.webp')}
-        contentFit="contain"
-      />
+      {showConfirm ? (
+        <Image
+          style={styles.image}
+          source={require('./assets/green_checkmark.webp')}
+          contentFit="contain"
+        />
+      ) : null}
     </View>
   );
 }
