094ebaa01d1c3ebdc9d590f7a24e9a05c5c9845a
Iarla Crewe <44999892+iarla-crewe@users.noreply.github.com>
2023-09-13 16:43:31 +0100

    Add green tick image

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Image } from 'expo-image';
 
 export default function App() {
+  // TODO - Implement server call code
+
+  // TODO - Implement beep sound effect
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        style={styles.image}
+        source="https://assets-global.website-files.com/60157e38af986c4fb2b69244/60f0ed18a47298cb2d9d8ab8_Eo_circle_green_checkmark.svg.webp"
+        contentFit="contain"
+      />
     </View>
   );
 }
@@ -17,4 +24,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    flex: 1,
+    width: "50%",
+    backgroundColor: '#fff'
+  }
 });
