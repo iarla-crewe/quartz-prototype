9c3d89357d72af1bfeab2a5ec54f35821e2ea8a0
Nelo0 <83479019+Nelo0@users.noreply.github.com>
2023-09-20 20:14:16 +0100

    Add timer on transaction starts its countdown based on the time the notification got sent

--- mobile-app/src/ui/screens/spend/SpendAcceptedScreen.tsx
+++ mobile-app/src/ui/screens/spend/SpendAcceptedScreen.tsx
@@ -14,8 +14,6 @@ export default function SpendAcceptedScreen( { route, navigation } : { route: an
     const { transactionDataJSON, sentTime } = route.params;
     const transactionData = CardTransactionData.fromJSON(transactionDataJSON);
 
-    console.log("Reference in spend accepted screen,", transactionData?.reference);
-
     const connection = createConnection();
     const wallet = getTestWallet();
     const provider = getProvider(connection, wallet);
@@ -24,8 +22,6 @@ export default function SpendAcceptedScreen( { route, navigation } : { route: an
     useEffect(() => {
         (async () => {
             let tx;
-
-            console.log("Spend Accept: ", transactionData);
             
             if (transactionData === undefined) {
                 console.log("Error: Deserialization of transaction data failed");
